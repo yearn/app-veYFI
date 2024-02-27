720085788d46db9aef01217f06a93b2cc74a18ea
Tim Reznichenko <tim.reznich@gmail.com>
2024-02-27 11:34:31 +0100

    chore: remove disabled state for unconnected wallets

--- app/components/ViewStakeUnstakeGauges.tsx
+++ app/components/ViewStakeUnstakeGauges.tsx
@@ -148,7 +148,6 @@ function StakeUnstakeButtons({vaultAddress, gaugeAddress, vaultDeposited, gaugeS
 }
 
 export function StakeUnstakeGauges(): ReactElement {
-	const {address} = useWeb3();
 	const {gaugesMap, userPositionInGauge} = useGauge();
 	const {vaults} = useYearn();
 	const {getBalance, getPrice} = useYearnWallet();
@@ -331,14 +330,8 @@ export function StakeUnstakeGauges(): ReactElement {
 										<Link
 											target={'_blank'}
 											rel={'noreferrer'}
-											href={
-												address
-													? `${process.env.YEARN_BASE_URI}/vaults/${VEYFI_CHAIN_ID}/${props.vaultAddress}`
-													: ''
-											}>
-											<Button
-												isDisabled={!address}
-												className={'h-8 w-full cursor-alias text-xs'}>
+											href={`${process.env.YEARN_BASE_URI}/vaults/${VEYFI_CHAIN_ID}/${props.vaultAddress}`}>
+											<Button className={'h-8 w-full cursor-alias text-xs'}>
 												{'Deposit in vault'}
 												<IconLinkOut className={'ml-2 size-4'} />
 											</Button>

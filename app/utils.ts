98edc101fca6b5a6e2559fa715480b0258233347
Ross <therossgalloway@gmail.com>
2025-06-02 11:35:58 -0400

    chore: update redemption contract

--- app/utils.ts
+++ app/utils.ts
@@ -9,7 +9,7 @@ import type {TAddress, TDict} from '@builtbymom/web3/types';
 export const MAX_LOCK: TSeconds = toSeconds(roundToWeek(YEAR * 4));
 export const VEYFI_CHAIN_ID = 1;
 export const VEYFI_REGISTRY_ADDRESS: TAddress = toAddress(''); // TODO: update once deployed
-export const VEYFI_OPTIONS_ADDRESS = toAddress('0x7dC3A74F0684fc026f9163C6D5c3C99fda2cf60a');
+export const VEYFI_OPTIONS_ADDRESS = toAddress('0x4707C855323545223fA2bA4150A83950F6F53b6E');
 export const VEYFI_DYFI_ADDRESS = toAddress('0x41252E8691e964f7DE35156B68493bAb6797a275');
 export const VEYFI_ADDRESS = toAddress('0x90c1f9220d90d3966FbeE24045EDd73E1d588aD5');
 export const VEYFI_POSITION_HELPER_ADDRESS = toAddress('0x5A70cD937bA3Daec8188E937E243fFa43d6ECbe8');
